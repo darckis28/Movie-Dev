db874fa953cab43756f731acc56d38a7b300f852
andi aguirre <aguirre280912@gmail.com>
2024-01-26 01:20:25 -0500

    finilization reponse page populary

---
+++ Movie-Dev/src/components/Containners/ContainnerRes.jsx
@@ -0,0 +1,9 @@
+function ContainnerRes({ children }) {
+  return (
+    <main className="py-9 text-white px-8  gap-3  m-auto grid grid-cols-1 md:grid-cols-4 md:w-4/5  ">
+      {children}
+    </main>
+  );
+}
+
+export default ContainnerRes;
---
+++ Movie-Dev/src/components/NextBtn/BtnsNexr.jsx
@@ -0,0 +1,43 @@
+import { GrPrevious, GrNext } from "react-icons/gr";
+function BtnsNexr({ page, setPage, direccion }) {
+  function handelNext() {
+    if (page <= 100) {
+      setPage(page + 1);
+      window.location.href = `#${direccion}`;
+    }
+  }
+  function handelBack() {
+    if (page > 1) {
+      setPage(page - 1);
+      window.location.href = `#${direccion}`;
+    }
+  }
+  return (
+    <div className="flex gap-5 w-full justify-center mb-16 ">
+      {page > 1 && (
+        <button
+          onClick={handelBack}
+          className="text-white rounded-full bg-gray-800 p-4   hover:bg-yellow-500"
+        >
+          <GrPrevious />
+        </button>
+      )}
+      <div className="text-white  px-3 py-2 bg-gray-700 rounded-full flex items-center">
+        <p className=" font-bold">
+          <span className=" rounded-full bg-gray-800 py-2 px-4 ">{page}</span>{" "}
+          de 100
+        </p>
+      </div>
+      {page <= 100 && (
+        <button
+          onClick={handelNext}
+          className="text-white rounded-full bg-gray-800 p-4 hover:bg-yellow-500"
+        >
+          <GrNext />
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default BtnsNexr;
